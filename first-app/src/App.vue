c1925fa6ef2807703f1781abb3fe1106b618de89
Amine Mesbahi <mesbahiamine886@gmail.com>
2022-03-21 20:52:09 +0100

    some exercies with vue

---
+++ first-app/src/App.vue
@@ -0,0 +1,65 @@
+<template>
+  <h1>{{ email }}</h1>
+  <div v-if="showHeader">
+    <HeaderNav  @edit="show">
+    <h1>Welcome to Header Template</h1>
+      <template v-slot:links>
+                  <a href="#">Sign up</a>
+                  <a href="#">view more</a>
+      </template>
+    </HeaderNav>
+    
+  </div>
+
+  <div v-if="showHeaderTwo">
+    <HeaderNav  @edit="showTwo">
+      <h1>Welcome to Vue js</h1>
+      <p>Helloooo there</p>
+    </HeaderNav>
+  </div>
+  
+  <button @click="show" >show</button>
+  <button @click="showTwo" >showTwo</button>
+  <button @click="count+=1">counter is: {{count}} </button>
+  <input type="email" name="" id="" v-model="email">
+</template>
+
+<script>
+import HeaderNav from "./components/HeaderNav.vue";
+
+export default {
+  name: 'App',
+  data(){
+    return{
+      count:0 ,
+      showHeader : false,
+      showHeaderTwo : false,
+      email:''
+    }
+  },
+  components:{
+    HeaderNav
+  },
+  methods:{
+    show(){
+      this.showHeader = !this.showHeader
+    },
+    showTwo(){
+      this.showHeaderTwo = !this.showHeaderTwo
+    },
+    
+
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ first-app/src/components/HeaderNav.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="container" @click.self="showTwo">
+        <div class="popUp">
+            <slot></slot>
+            <slot name="links"></slot>
+        </div>
+        
+    </div>
+</template>
+
+<script>
+export default {
+    name: "HeaderNav",
+    props: ['title','edit'],
+    methods:{
+        showTwo(){
+            this.$emit('edit')
+        }
+    }
+}
+</script>
+
+
+<style scoped>
+.container{
+    width: 100%;
+    height: 100vh;
+    background-color: rgba(0, 0, 0, 0.377);
+    position: fixed;
+    left: 0;
+    top: 0;
+
+}
+.popUp{
+    margin: auto;
+    margin-top:50px;
+    width: 400px;
+    height: 300px;
+    background-color: white;
+    border-radius: 10px;
+}
+</style>
